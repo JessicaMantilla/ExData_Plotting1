dfa3aab4c86904152fced81e9d8bb7d39bdbc69a
JessicaMantilla <68915984+JessicaMantilla@users.noreply.github.com>
2020-08-10 14:51:06 -0500

    Add files via upload
    This is the 3° script (plot3.R)

---
+++ plot3.R
@@ -0,0 +1,7 @@
+#Plot3.R
+png(file = "plot3.png",width = 480, height = 480)
+plot(datetime,power$Sub_metering_1,type="l",ylab="Energy sub metering",xlab="")
+points(datetime,power$Sub_metering_2,type="l",col="red")
+points(datetime,power$Sub_metering_3,type="l",col="blue")
+legend("topright", cex = 1,lty=c(1,1,1),col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+dev.off()
